1d9d9cd038dff8790a17ef0a67c6327d3c0d217c
bigmenflies <83069356+bigmenflies@users.noreply.github.com>
2021-04-23 07:21:59 -0400

    Initial commit

---
+++ src/js/validate.js
@@ -0,0 +1,37 @@
+function validate(event) {
+
+  var lat = document.getElementById('latitude').value;
+  var lng = document.getElementById('longitude').value;
+  const valid = true;
+
+  var errLat = document.getElementById('errorLat');
+  var errLng = document.getElementById('errorLng');
+
+  if(valid === true){
+
+    if(!(lat < -90 && lat > 90)){
+      valid = false;
+      errLat.innerText = "must be a valid Latitude (-90 to 90)";
+    }
+    else{
+      errLat.innerText = "";
+    }
+
+    if(!(lng < -180 && lng > 180)){
+      valid = false;
+      errLng.innerText = "must be a valid Longitude (-180 to 180)";
+    }
+    else{
+      errLng.innerText = "";
+    }
+  }
+  if(valid === false){
+    event.preventDefault();
+  }
+};
+
+// Wait for the window to load, then set up the submit event handler for the form.
+window.onload = function() {
+  const form = document.querySelector('form');
+  form.onsubmit = validate;
+};
